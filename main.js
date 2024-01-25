34ce9ee34f2017e031ed080d927d0ffdd1896005
Nurbekprog <nurprogdata@gmail.com>
2024-01-25 19:49:37 +0500

    finish

--- main.js
+++ main.js
@@ -6,3 +6,19 @@ const toggleNav = () => {
   mobileNav.classList.toggle("hamburger-active");
 };
 mobileNav.addEventListener("click", () => toggleNav());
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.classList.add("backtop-show");
+  } else {
+    backtop.classList.remove("backtop-show");
+  }
+}
+
+const checkbox = document.getElementById("checkbox");
+checkbox.addEventListener("change", () => {
+  document.body.classList.theme-toggle-button("dark");
+});
