90be8903b093271a2f3d225576096cb6b08b3439
Nurbekprog <nurprogdata@gmail.com>
2024-01-27 03:26:41 +0500

    finallllly

--- main.js
+++ main.js
@@ -41,3 +41,13 @@ setTimeout(() => {
   loading.classList.add("loading-none");
 }, loadingDuration);
 
+let modeBtn = document.getElementById("mode-btn");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "./assets/images/light.svg";
+  } else {
+    this.firstElementChild.src = "./assets/images/dark.svg";
+  }
+  document.body.classList.toggle("dark");
+});
